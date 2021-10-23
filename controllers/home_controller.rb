830cdb552de6b354fb6d85b5cf3cfdfc2db5ae43
Tyo <cheertrain15@naver.com>
2021-10-24 00:48:26 +0900

    Added personal ROR files via CLI

---
+++ controllers/home_controller.rb
@@ -0,0 +1,11 @@
+class HomeController < ApplicationController
+  def index
+    puts "'Index' is called!"
+  end
+  def about
+    puts "'About' is called!"
+  end
+  def service
+    puts "'Service' is called!"
+  end
+end
