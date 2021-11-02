188eaee23dacd775e5abdff46aa26572dafaeafc
tyomhk <cheertrain15@naver.com>
2021-11-02 22:00:47 +0900

    Tested referencing intance variables from home controller, and referencing them on the 'about' view.

--- controllers/home_controller.rb
+++ controllers/home_controller.rb
@@ -4,6 +4,9 @@ class HomeController < ApplicationController
   end
   def about
     puts "'About' is called!"
+    # Instance variable. Can be referenced anywhere ?
+    @about_me = "What is my name?"
+    @about_number = 777
   end
   def service
     puts "'Service' is called!"
